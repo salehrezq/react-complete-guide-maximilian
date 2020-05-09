6f9a78eaa36e5c4630fb7db321332c2ba733e750
Saleh Rezq <salehrezq@gmail.com>
2020-05-09 21:49:46 +0200

    Person component with dynamic content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi I am a react app</h1>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+
     </div>
   );
 }
