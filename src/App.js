c1dc90615ba1d1f1b724ee33406c296714d4d1cb
Saleh Rezq <salehrezq@gmail.com>
2020-05-10 00:40:50 +0200

    Change the state and re render the DOM to reflect

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component{
+
+    state = {
+        persons: [
+            {name: 'Saleh', age: 33},
+            {name: 'Ramy', age: 28},
+            {name: 'Shrod', age: 25}
+        ]
+    }
+
+    switchNameHandler = () => {
+       this.setState({
+           persons: [
+               {name: 'Saleh Rezq', age: 33},
+               {name: 'Ramy', age: 28},
+               {name: 'Shrod', age: 10}
+           ]
+       });
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>Hi I am a react app</h1>
-                <Person name="Saleh" age="32"/>
-                <Person name="Mol" age="30">
-                    Child content
+                <h1>Hi I am a React app</h1>
+                <button onClick={this.switchNameHandler}>Switch</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+                Child content
                 </Person>
             </div>
         );
