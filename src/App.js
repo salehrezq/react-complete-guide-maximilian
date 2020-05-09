ea2dfe7740c87051967dd68e1e2c8e3eeb603248
Saleh Rezq <salehrezq@gmail.com>
2020-05-09 22:24:48 +0200

    Switch from functional React to class React
    This switch so that I can follow the course structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I am a react app</h1>
-        <Person name="Saleh" age="32"/>
-        <Person name="Mol" age="30">
-            Child content8
-        </Person>
-
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I am a react app</h1>
+                <Person name="Saleh" age="32"/>
+                <Person name="Mol" age="30">
+                    Child content
+                </Person>
+            </div>
+        );
+    }
 }
 
 export default App;
