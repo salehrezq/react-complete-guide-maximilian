9275c14abfde7f425e40a64491e453f3eec2e9a4
Saleh Rezq <salehrezq@gmail.com>
2020-05-11 22:39:49 +0200

    This commit is a reversion to a previous commit
    An example commit before this was:
    Switch to functional React as example
    b4a7ffe85602be5d193d3354eb8827087769fd98
    
    Now we revert to the state before that example commit
    to an older commit which is this commit:
    Change the state and re render the DOM to reflect
    c1dc90615ba1d1f1b724ee33406c296714d4d1cb

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
+class App extends Component{
 
-    const [personsState, setPersonsState] = useState({
+    state = {
         persons: [
             {name: 'Saleh', age: 33},
             {name: 'Ramy', age: 28},
             {name: 'Shrod', age: 25}
         ]
-    });
-
-    const [otherState, setOtherState] = useState('some other value');
-
-    console.log(personsState, otherState);
-    
-   const switchNameHandler = () => {
-       setPersonsState({
-            persons: [
-                {name: 'Saleh Rezq', age: 33},
-                {name: 'Ramy', age: 28},
-                {name: 'Shrod', age: 10}
-            ]
-        });
     }
 
-   return (
-        <div className="App">
-            <h1>Hi I am a React app</h1>
-            <button onClick={switchNameHandler}>Switch</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
-            Child content
-            </Person>
-        </div>
-   );
+    switchNameHandler = () => {
+       this.setState({
+           persons: [
+               {name: 'Saleh Rezq', age: 33},
+               {name: 'Ramy', age: 28},
+               {name: 'Shrod', age: 10}
+           ]
+       });
+    }
 
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I am a React app</h1>
+                <button onClick={this.switchNameHandler}>Switch</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+                Child content
+                </Person>
+            </div>
+        );
+    }
 }
 
 export default App;
