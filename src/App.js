8ddbec5deb1791967d65d3165065200c0ea78708
Saleh Rezq <salehrezq@gmail.com>
2020-05-12 18:21:17 +0200

    Add inline style
    Lesson:
    03. Understanding the Base Features & Syntax\
    25. Working with Inline Styles

--- src/App.js
+++ src/App.js
@@ -33,13 +33,22 @@ class App extends Component{
     }
 
     render() {
+
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Hi I am a React app</h1>
                 {/* Using arrow function approach - versus bind() - can cause react
                     to re-render certain things in your app too often; causes performance issues.
                     arrow function approach is not the recommended approach */}
-                <button onClick={(event) => this.switchNameHandler("Saleh Rezq")}>Switch</button>
+                <button style={style} onClick={(event) => this.switchNameHandler("Saleh Rezq")}>Switch</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 {/* `click` and `changed` passed to <Person> are words chosen by you and can be anything */}
                 <Person click={this.switchNameHandler.bind(this, "Saleh!!!!")} name={this.state.persons[1].name} age={this.state.persons[1].age}/>
