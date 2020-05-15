69aafa2a46df8f7939334c52224608df96a24d22
Saleh Rezq <salehrezq@gmail.com>
2020-05-15 15:43:50 +0200

    Outputting lists using loop or map() function
    Lesson:
    04. Working with Lists and Conditionals\
    05. Outputting Lists
    
    Warning: Each child in a list should have a unique "key" prop.
    
    Check the render method of `App`. See https://fb.me/react-warning-keys
    for more information.
        in person (at App.js:58)
        in App (at src/index.js:9)
        in StrictMode (at src/index.js:8)

--- src/App.js
+++ src/App.js
@@ -54,18 +54,10 @@ class App extends Component{
         if(this.state.showPersons){
             persons = (
                 <div>
-                    <Person name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}/>
-                    {/* `click` and `changed` passed to <Person> are words chosen by you and can be anything */}
-                    <Person click={this.switchNameHandler.bind(this, "Saleh!!!!")}
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}/>
-                    <Person
-                        changed={this.nameChangeHandler}
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}>
-                        Child content
-                    </Person>
+                    {this.state.persons.map(person => {
+                        return  <Person name={person.name}
+                                        age={person.age}/>
+                    })}
                 </div>
             );
         }
