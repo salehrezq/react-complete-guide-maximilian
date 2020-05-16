370c25a14a64f5a1e0a32f21daa4456b5d295b43
Saleh Rezq <salehrezq@gmail.com>
2020-05-16 13:37:59 +0200

    Initial implementation for CharComponent component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import ValidationComponent from './ValidationComponent/ValidationComponent'
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component{
 
@@ -18,6 +19,7 @@ class App extends Component{
           <input type="text" onChange={event => this.updatedInput(event)} value={this.state.content}/>
           <p>{this.state.content.length}</p>
           <ValidationComponent textLength={this.state.content.length}/>
+          <CharComponent/>
       </div>
     )
   }
---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const charComponent = props => {
+
+    const style = {
+        display: 'inline-block',
+        padding: '16px',
+        textAlign: 'center',
+        margin: '16px',
+        border: '1px solid black'
+    };
+
+    return (
+        <div className="App">
+            <p style={style}>Hi</p>
+        </div>
+    );
+}
+
+export default charComponent;
