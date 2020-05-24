2bff27e6369fb2bdba11468924018a09e07afc8e
Saleh Rezq <salehrezq@gmail.com>
2020-05-25 01:09:44 +0200

    Convert components from functional to class
    Lesson:
    07. Diving Deeper into Components & React Internals\
    08. Component Update Lifecycle (for props Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Person.css';
 import personStyles from './Person.module.css';
 
 
+class Person extends Component{
 
-const person = (props) => {
+render() {
 
     console.log('[Person.js] rendering...');
-
     return (
         <div className={personStyles.Person}>
-            <p onClick={props.click} >I'm a {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
+            <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>
+            <p>{this.props.children}</p>
             {/* You will receive a Warning because you provided a `value` prop to a form
                 field without applying a `changed` prop to all <Person> components */}
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
         </div>
-    )
-};
+    );
+};}
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-// In ES6 syntax; if arrow function starts with the statement omitting the curly braces {}
-// Then simply it implicitly achieves a `return` to that statement
-// Multiple lines are allowed in that returned statement.
-const persons = props => {
+class Persons extends Component {
 
-    console.log('[Persons.js] rendering...');
+    render() {
 
-    return(props.persons.map((person, index) => {
-        return  <Person name={person.name}
-                        age={person.age}
-                        click={() => props.deleteOnClick(index)}
-                        key={person.id}
-                        changed={event => props.nameChanged(event, person.id)}/>
-    })
-)};
+        console.log('[Persons.js] rendering...');
+        return (this.props.persons.map((person, index) => {
+            return  <Person name={person.name}
+                            age={person.age}
+                            click={() => this.props.deleteOnClick(index)}
+                            key={person.id}
+                            changed={event => this.props.nameChanged(event, person.id)}/>
+        }));
+    }
+}
 
-
-export default persons;
+export default Persons;
