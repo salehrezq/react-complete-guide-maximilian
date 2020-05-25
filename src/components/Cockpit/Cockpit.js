fb67e2edd23bc205ba557c26fd9b36223845dbd3
Saleh Rezq <salehrezq@gmail.com>
2020-05-26 00:45:59 +0200

    Controlling the useEffect() behavior
    Lesson:
    07. Diving Deeper into Components & React Internals\
    11. Controlling the useEffect() Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,9 +4,24 @@ import classes from './Cockpit.module.css';
 
 const Cockpit = props => {
 
+
+    // The default behavior for useEffect() is to fire the effect after every completed render.
+    // Second argument of useEffect() is an array of the props you want the effect to depend on.
+    // In this example; when props.persons changes then fire the effect.
+    //
+    // If you want to run an effect only once
+    // you can pass an empty array ([]) as a second argument.
+    // This tells React that your effect does not depend on any values from props or state,
+    // so it never needs to re-run.
+    // This isn’t handled as a special case — it follows
+    // directly from how the dependencies array always works.
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-    });
+        // Http request...
+        setTimeout(() => {
+            alert("Save data to cloud!");
+        }, 1000);
+    }, [props.persons]);
 
     const classNames = [];
     if(props.persons.length <= 2){
