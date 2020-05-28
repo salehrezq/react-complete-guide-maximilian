bbccb763bb96b799a04223f9a435f467edf7d6cd
Saleh Rezq <salehrezq@gmail.com>
2020-05-29 00:02:25 +0200

    Adjacent JSX elements using array with keys
    Lesson:
    07. Diving Deeper into Components & React Internals\
    19. Rendering Adjacent JSX Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,13 +9,11 @@ render() {
 
     console.log('[Person.js] rendering...');
     return (
-        <div className={personStyles.Person}>
-            <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>
-            <p>{this.props.children}</p>
-            {/* You will receive a Warning because you provided a `value` prop to a form
-                field without applying a `changed` prop to all <Person> components */}
-            <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </div>
+            [
+            <p key="i1" onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>,
+            <p key="i2">{this.props.children}</p>,
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+            ]
     );
 };}
 
