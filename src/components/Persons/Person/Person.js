c1392aec4c3e12750660005ecc542bea0e2cdc00
Saleh Rezq <salehrezq@gmail.com>
2020-05-29 01:46:20 +0200

    Implement Auxiliary.js comp for wrapping purposes
    Lesson:
    07. Diving Deeper into Components & React Internals\
    19. Rendering Adjacent JSX Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Person.css';
 import personStyles from './Person.module.css';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 
 class Person extends Component{
@@ -9,11 +10,11 @@ render() {
 
     console.log('[Person.js] rendering...');
     return (
-            [
-            <p key="i1" onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>,
-            <p key="i2">{this.props.children}</p>,
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-            ]
+        <Auxiliary>
+            <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        </Auxiliary>
     );
 };}
 
