c486cd8148101d8c6531df4a8918adcd032b6352
Saleh Rezq <salehrezq@gmail.com>
2020-05-29 02:57:21 +0200

    Using React.Fragment for wrapping purposes
    Lesson:
    07. Diving Deeper into Components & React Internals\
    21. Using React.Fragment
    
    Since React 16.2
    To allow adjacent JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './Person.css';
 import personStyles from './Person.module.css';
-import Auxiliary from '../../../hoc/Auxiliary';
 
 
 class Person extends Component{
@@ -10,11 +9,11 @@ render() {
 
     console.log('[Person.js] rendering...');
     return (
-        <Auxiliary>
+        <Fragment>
             <p onClick={this.props.click} >I'm a {this.props.name} and I am {this.props.age} years old</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </Auxiliary>
+        </Fragment>
     );
 };}
 
